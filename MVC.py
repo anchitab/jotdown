50d7f07e0f18482e6ccbf3f1b502b25089b3587b
Jeana-T <jeana2019@gmail.com>
2020-12-04 16:27:30 -0800

    Create base classes for View, Model, Controller

---
+++ MVC.py
@@ -0,0 +1,19 @@
+import tkinter 
+import os     
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from tkinter import font, filedialog
+from tkhtmlview import HTMLLabel
+
+from tkinter import messagebox as mbox
+from markdown2 import Markdown
+
+class View():
+    pass
+
+class Model():   
+    pass
+    
+class Controller():
+    pass
