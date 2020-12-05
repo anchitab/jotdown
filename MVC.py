7e9ccc48c17897e39b5339753c98b4090270845a
Dhruvi Faria <fariadhruvi@gmail.com>
2020-12-05 12:56:51 -0800

    imported View for Command

--- MVC.py
+++ MVC.py
@@ -1,5 +1,9 @@
 import tkinter 
-import os     
+import os 
+
+import command
+from command import *
+
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
@@ -15,7 +19,7 @@ class View(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.master = master  
+        self.master = master
         self.init_window(master)
 		# self.invoker = Invoker()
         
@@ -110,102 +114,6 @@ class Model():
         html = self.md2html.convert(markdownText)        
         return html
 
-class Command(View):
-	def execute(self) -> None:
-	    pass 
-
-class OpenFileCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-		self.file = askopenfilename(defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")])
-		
-		if self.file == "": 
-			# if there is no file to open 
-			self.file = None
-		
-		else: 	
-			# Open the file 
-			# Change window title 
-			self.master.title(os.path.basename(self.file) + " - Jotdown") 
-			self.inputEditor.delete(1.0,END)
-
-			file = open(self.file,"r") 
-
-			self.inputEditor.insert(1.0,file.read())
-
-			file.close() 
-
-class NewFileCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-		# print("reachednewfile")
-		self.file = None
-		self.inputEditor.delete(1.0,END)
-
-class SaveFileCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-		if self.file == None: 
-			# Save as new file + name file
-			self.file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")]) 
-
-			if self.file == "": 
-				self.file = None
-			else: 
-				# Save the file 
-				file = open(self.file,"w") 
-				file.write(self.inputEditor.get(1.0,END)) 
-				file.close() 
-				
-				# Set window title 
-				self.master.title(os.path.basename(self.file) + " - Jotdown") 
-		
-		# If file already named save using that name (does not ask for user input)	
-		else: 
-			file = open(self.file,"w") 
-			file.write(self.inputEditor.get(1.0,END))
-			file.close()
-
-class CopyCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-	    self.inputEditor.event_generate("<<Copy>>")
-
-class CutCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-	    self.inputEditor.event_generate("<<Cut>>")
-
-class PasteCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-	    self.inputEditor.event_generate("<<Paste>>")
-
-class UndoCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-	    self.inputEditor.event_generate("<<Undo>>")
-
-class RedoCommand(Command):
-	def __init__(self, inputEditor: Text) -> None:
-		self.inputEditor = inputEditor
-
-	def execute(self) -> None:
-	    self.inputEditor.event_generate("<<Redo>>")
 
 class Controller():
 	def __init__(self, view:View, model:Model) -> None:
---
+++ command.py
@@ -0,0 +1,111 @@
+import tkinter 
+import os 
+
+import MVC
+from MVC import View
+
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from tkinter import font, filedialog
+from tkhtmlview import HTMLLabel
+
+from tkinter import messagebox as mbox
+from markdown2 import Markdown
+
+class Command(View):
+	def execute(self) -> None:
+	    pass 
+
+class OpenFileCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+		self.file = askopenfilename(defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")])
+		
+		if self.file == "": 
+			# if there is no file to open 
+			self.file = None
+		
+		else: 	
+			# Open the file 
+			# Change window title 
+			self.master.title(os.path.basename(self.file) + " - Jotdown") 
+			self.inputEditor.delete(1.0,END)
+
+			file = open(self.file,"r") 
+
+			self.inputEditor.insert(1.0,file.read())
+
+			file.close() 
+
+class NewFileCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+		# print("reachednewfile")
+		self.file = None
+		self.inputEditor.delete(1.0,END)
+
+class SaveFileCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+		if self.file == None: 
+			# Save as new file + name file
+			self.file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")]) 
+
+			if self.file == "": 
+				self.file = None
+			else: 
+				# Save the file 
+				file = open(self.file,"w") 
+				file.write(self.inputEditor.get(1.0,END)) 
+				file.close() 
+				
+				# Set window title 
+				self.master.title(os.path.basename(self.file) + " - Jotdown") 
+		
+		# If file already named save using that name (does not ask for user input)	
+		else: 
+			file = open(self.file,"w") 
+			file.write(self.inputEditor.get(1.0,END))
+			file.close()
+
+class CopyCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+	    self.inputEditor.event_generate("<<Copy>>")
+
+class CutCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+	    self.inputEditor.event_generate("<<Cut>>")
+
+class PasteCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+	    self.inputEditor.event_generate("<<Paste>>")
+
+class UndoCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+	    self.inputEditor.event_generate("<<Undo>>")
+
+class RedoCommand(Command):
+	def __init__(self, inputEditor: Text) -> None:
+		self.inputEditor = inputEditor
+
+	def execute(self) -> None:
+	    self.inputEditor.event_generate("<<Redo>>")
