9e763b142495a184502601d20305ae875dd15c2f
Dhruvi Faria <fariadhruvi@gmail.com>
2020-12-09 15:03:56 -0800

    Testing against testoutput

--- test.py
+++ test.py
@@ -1,39 +1,41 @@
-import tkinter 
-import os     
-from tkinter import *
-from tkinter.messagebox import *
-from tkinter.filedialog import *
-from tkinter import font, filedialog
-from tkhtmlview import HTMLLabel
+# import tkinter 
+# import os     
+# from tkinter import *
+# from tkinter.messagebox import *
+# from tkinter.filedialog import *
+# from tkinter import font, filedialog
+# from tkhtmlview import HTMLLabel
 
-from tkinter import messagebox as mbox
-from markdown2 import Markdown
+# from tkinter import messagebox as mbox
+# from markdown2 import Markdown
 
-import unittest
-import MVC
-from MVC import *
+# import unittest
+# import MVC
+# from MVC import *
 
-class TestInput(unittest.TestCase):
+# class TestInput(unittest.TestCase):
 
-     def test(self, view:View):
-        testString = "hi crocodile"
-        # self.view = View()
-        test = view.inputeditor(testString)
-        self.assertEqual(test,"hi crocodile")
+#      def test(self):
+#         testString = "hi crocodile"
+#         # self.view = View()
+#         testInput = view.inputeditor(testString)
+#         testOutput = model.getHTML(testInput)
+#         self.assertEqual(testInput, testOutput)
 
-    # def testBold(self):
-    #     testString = '**hi crocodile**'
-    #     # self.view = View()
-    #     boldString = HTMLLabel(text='hi crocodile', font='bold')
-    #     test = model.getHTML(testString)
-    #     self.assertEqual(test, boldString)
+#     # def testBold(self):
+#     #     testString = '**hi crocodile**'
+#     #     # self.view = View()
+#     #     boldString = HTMLLabel(text='hi crocodile', font='bold')
+#     #     test = model.getHTML(testString)
+#     #     self.assertEqual(test, boldString)
 
-# root = Tk()
-# view = View(root)
 
-if __name__ == '__main__':
-    model = Model()
-    unittest.main()
+
+# if __name__ == '__main__':
+#     root = Tk()
+#     model = Model()
+#     view = View(root)
+#     unittest.main()
 
 
 
