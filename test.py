e31669dc3677c9d68d468b3f4da220668f1bc8e8
Dhruvi Faria <fariadhruvi@gmail.com>
2020-12-10 13:42:00 -0800

    need to fix input test

--- test.py
+++ test.py
@@ -1,44 +1,89 @@
-# import tkinter 
-# import os     
-# from tkinter import *
-# from tkinter.messagebox import *
-# from tkinter.filedialog import *
-# from tkinter import font, filedialog
-# from tkhtmlview import HTMLLabel
+import tkinter 
+import os     
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from tkinter import font, filedialog
+from tkhtmlview import HTMLLabel
 
-# from tkinter import messagebox as mbox
-# from markdown2 import Markdown
+from tkinter import messagebox as mbox
+from markdown2 import Markdown
 
-# import unittest
-# import MVC
-# from MVC import *
+import unittest
+import MVC
+from MVC import *
+from MVC import View
+from MVC import Model
 
-# class TestInput(unittest.TestCase):
+# # WORKING BASE TEST
+# class GUITestCase(unittest.TestCase):
+#     def test_helloworld(self):
+#         testInput = "hello world"
+#         self.assertEqual(testInput, "hello world")
 
-#      def test(self):
-#         testString = "hi crocodile"
-#         # self.view = View()
-#         testInput = view.inputeditor(testString)
-#         testOutput = model.getHTML(testInput)
-#         self.assertEqual(testInput, testOutput)
-
-#     # def testBold(self):
-#     #     testString = '**hi crocodile**'
-#     #     # self.view = View()
-#     #     boldString = HTMLLabel(text='hi crocodile', font='bold')
-#     #     test = model.getHTML(testString)
-#     #     self.assertEqual(test, boldString)
-
-
-
-# if __name__ == '__main__':
-#     root = Tk()
-#     model = Model()
-#     view = View(root)
+# # run all tests from all sets
+# if __name__ == "__main__":
 #     unittest.main()
 
+class TestInput(unittest.TestCase):
 
+    def test_Input(self):
+        testString = 'hi crocodile'
+        root = Tk()
+        view = View(root)
+        view.inputeditor.insert('1.0', testString)
+        # view.inputeditor.edit_modified(1)
+        view.inputeditor.event_generate("<<Modified>>")
+        self.assertEqual(view.outputbox.get(1.0, END), testString)
 
+ 
+if __name__ == '__main__':
+    # root = Tk()
+    # # model = Model()
+    # view = View(root)
+    unittest.main()
+
+# def test_Bold(self):
+#         testString = '**hi crocodile**'
+#         # self.view = View()
+#         boldString = HTMLLabel(text='hi crocodile', font='bold')
+#         test = model.getHTML(testString)
+#         self.assertEqual(test, boldString)
+
+   # def test_input(self):
+    #     testString = "hi crocodile"
+    #     root = Tk()
+    #     self.view = View(root)
+    #     # self.model = Model()
+    #     testInput = view.inputeditor.set(testString)
+    #     testOutput = view.outputbox.get(1.0, END)
+    #     self.assertEqual(testInput, testOutput)
 
    
 
+    # def test(self):
+    #     testString = "hi crocodile"
+    #     # self.view = view
+    #     # self.model = model
+    #     # self.view = View()
+    #     # self.model= Model()
+    #     testInput = view.inputeditor.get(testString)
+    #     testOutput = model.getHTML(testInput)
+    #     self.assertEqual(testInput, testOutput)
+
+    # from mock import Mock
+
+# class GUITestCase(unittest.TestCase):
+#     def setUp(self):
+#         root = Tk()
+#         self.view = View(root)
+
+#     # def tearDown(self):
+#     #     view.destroy()
+
+#     def test_geometry(self):
+#         root.winfo_geometry()
+
+# # run all tests from all sets
+# if __name__ == "__main__":
+#     unittest.main()
