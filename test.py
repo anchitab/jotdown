27da5e2ed74a4c151440caea58f9419592683e24
Dhruvi Faria <fariadhruvi@gmail.com>
2020-12-10 21:41:25 -0800

    tested integer input

--- test.py
+++ test.py
@@ -44,6 +44,14 @@ class TestInput(TKinterTestCase):
         self.pumpEvents()
         self.assertEqual(self.view.outputbox.get("1.0", END), inputString)
 
+    def test_Spaces(self):
+        inputString = 'hi   crocodile' + '\n'
+        self.pumpEvents()
+        self.view.inputeditor.insert('1.0', inputString)
+        self.pumpEvents()
+        self.assertEqual(self.view.outputbox.get("1.0", END), inputString)
+
+
  
 if __name__ == '__main__':
     unittest.main()
