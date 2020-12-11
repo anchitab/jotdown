1d4258fcb7553a169bb041013c96ba9de96bc086
Anchita Bora <anchita.bora@gmail.com>
2020-12-10 22:22:25 -0800

    added test_decimalsAndChars

--- test.py
+++ test.py
@@ -129,7 +129,12 @@ class TestInput(TKinterTestCase):
         self.pumpEvents()
         self.assertEqual(self.view.outputbox.get("1.0", END), inputString)
 
-
+    def test_decimalsAndChars(self):
+        inputString = '3.1415 pi' + '\n'
+        self.pumpEvents()
+        self.view.inputeditor.insert('1.0', inputString)
+        self.pumpEvents()
+        self.assertEqual(self.view.outputbox.get("1.0", END), inputString)
  
 if __name__ == '__main__':
     unittest.main()
